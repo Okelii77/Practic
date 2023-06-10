18142d4cbfa91a316909300c13377cb6dea531ea
Oleg <oleg2003ruslanov@gmail.com>
2023-06-10 22:06:52 +0300

    Русланов Олег
    1 работа по практике

---
+++ SampleProject/SampleProject/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace SampleProject;
+
+public class Person
+{
+
+    //field
+    private string _name;
+
+
+    //property
+    public int Id 
+    {
+        // getter - метод, позволяющий получить значение свойства 
+        get;
+        // setter - метод, позволяющий задать значение свойства 
+        set;
+    }
+    public Person(int id, string name)
+    {
+        Id = id;
+        _name = name;
+    }
+
+    //method
+    public string GetName()
+    {
+        return _name;
+    }
+}
+
---
+++ SampleProject/SampleProject/Program.cs
@@ -0,0 +1,21 @@
+﻿//int number = 10;
+//var number2 = 2;
+
+//string str = "some string";
+
+//Console.WriteLine( $"Number is {number}");
+//Console.WriteLine( "Number is " + number);
+
+//string stringFromConsole = Console.ReadLine();
+
+//Console.WriteLine(stringFromConsole);
+
+using SampleProject;
+
+
+Person personWithName = new Person( 1241, "Oleg");
+
+
+Console.WriteLine(personWithName.Id);
+
+
